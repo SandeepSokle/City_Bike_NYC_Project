33131fed83e1b52b227b56e0df0680d5923051fd
SandeepSokle <60534028+SandeepSokle@users.noreply.github.com>
2021-09-05 10:36:47 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,24 +7,11 @@
 import Filter from "./Filter";
 import Table from "./Table";
 
-import { useState,useEffect } from "react";
+import { useState } from "react";
 import "./CSS/Home.css"
 let Home = (props) => {
 
   let [filter,setFilter] = useState("all");
-
-  // useEffect(() => {
-  //   fetch("/data")
-  //     .then((e) => {
-  //       // console.log(e.json());
-  //       return e.json();
-  //     }).then((data)=>{
-  //       props.setData(data);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }, []);
 
   return (
     <div className="home-Container col-12">
