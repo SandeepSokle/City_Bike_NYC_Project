724dbc8e758f4b7e8ff0ce937e36062831bfdab5
SandeepSokle <60534028+SandeepSokle@users.noreply.github.com>
2021-09-05 10:38:09 +0530

    Update Payment.jsx

--- src/Payment.jsx
+++ src/Payment.jsx
@@ -7,7 +7,7 @@ import "./CSS/Payment.css"
 
 let Payment = ()=>{
     return (<div className = "Payment_Container">
-        <img className = "barCode" src = "./Images/payBar.jpeg" width="500" height="600"/>
+        <img className = "barCode" alt = "Pay" src = "./Images/payBar.jpeg" width="500" height="600"/>
     </div>)
 }
 
