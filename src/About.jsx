82fd6b34d46094d229ce490374f99eb7ac408655
SandeepSokle <60534028+SandeepSokle@users.noreply.github.com>
2021-09-06 14:36:42 +0530

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -9,14 +9,18 @@ let About = ()=>{
     <>
 
     <div className = "fs-2">
-`Note: For this coding example, you will need to install a Chrome extension called “CORS Unblock”, s
-o that you can use an external 3rd party API without running Cross Origin issues. 
-
+<h2>`Note: For this coding example, you will need to install a Chrome extension called “CORS Unblock”, s
+o that you can use an external 3rd party API without running Cross Origin issues. </h2>
+<br></br>
 Install the Chrome extension here: `
 <a href = "https://chrome.google.com/webstore/detail/cors-unblock/lfhmikememgdcahcdlaciloancbhjino/related?hl=en">Link of Extension </a>
     </div>
 
     <div className = "fs-3">
+<br></br>
+        
+<br></br>
+        
         "Citi Bike is the nation's largest bike share program, with 5,362 bikes and over 509 stations across The City of New York's. It was designed for quick trips with convenience in mind, and it’s a fun and affordable way to get around town."
     </div>
     
